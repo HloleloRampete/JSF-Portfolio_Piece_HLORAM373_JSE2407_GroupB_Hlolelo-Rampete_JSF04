18e9995f132a9ddc7e9900327c7e429e72bc6d95
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-08-08 15:01:24 +0200

    "Installed all dependencies and added project files."

---
+++ src/views/CartPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <!-- Add product listing here -->
+  </div>
+</template>
+
+<script>
+
+
+export default {};
+</script>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+
+    <!-- Add product listing here -->
+  </div>
+</template>
+
+<script>
+
+
+export default {
+};
+</script>
