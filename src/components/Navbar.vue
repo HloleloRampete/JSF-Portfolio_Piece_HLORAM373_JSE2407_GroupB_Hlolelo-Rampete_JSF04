4ec17c45ef022c06e44ac506fbb786376f586128
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-08-13 11:11:16 +0200

    "Wrapped the Cart SVG in an <a> tag: This makes the SVG clickable, and it will now behave like a link, navigating the user to the /cart page when clicked"

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -37,7 +37,7 @@
         class="w-full md:block md:w-auto"
         id="navbar-default"
       >
-        <ul
+      <ul
           class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-500 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0"
         >
           <li>
@@ -52,25 +52,27 @@
                 2
               </p>
             </div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="h-6 w-6 stroke-white cursor-pointer"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-              ></path>
-            </svg>
+            <a href="/cart">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="h-6 w-6 stroke-white cursor-pointer"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                ></path>
+              </svg>
+            </a>
           </li>
           <li>
             <a
               class="lg:hidden md:hidden py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-              href="/"
+              href="/cart"
             >Cart</a>
           </li>
           <li v-if="isLoggedIn">
@@ -94,7 +96,6 @@
 </template>
 
 <script>
-import { useRouter } from 'vue-router';
 
 export default {
   name: "Navbar",
