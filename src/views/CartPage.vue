4e6712483fa08c19cbc5c36b45d1cdd97c775746
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-08-16 14:11:21 +0200

    "Removed conditional rendering of cart items and removed item-specific data from cart item template"

--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -1,18 +1,11 @@
 <template>
   <div class="container mx-auto p-6">
     <h1 class="text-2xl font-bold mb-4">Your Cart</h1>
-    <div v-if="cartItems.length > 0" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      <div v-for="item in cartItems" :key="item.product.id" class="flex flex-col bg-white p-4 rounded-lg shadow">
-        <img :src="item.product.image" alt="Product Image" class="object-contain h-32 mb-4">
-        <div class="flex-1 flex flex-col justify-between">
-          <h2 class="text-lg font-semibold mb-2">{{ item.product.title }}</h2>
-          <p class="text-slate-500 mb-2">{{ item.product.category }}</p>
-          <p class="font-bold text-lg mb-2">${{ item.product.price }}</p>
-          <p class="text-slate-600">Quantity: {{ item.quantity }}</p>
-        </div>
+    <div  class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div class="flex flex-col bg-white p-4 rounded-lg shadow">
       </div>
     </div>
-    <div v-else>
+    <div>
       <p>Your cart is empty.</p>
     </div>
     <div class="mt-6">
