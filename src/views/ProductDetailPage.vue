059d144341fb099f0b559ff9ec0ed6caedf08aa5
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-08-19 13:36:07 +0200

    "Refactored ProductDetail and ProductDetailPage components: updated layout, styling, and button design."

--- src/views/ProductDetailPage.vue
+++ src/views/ProductDetailPage.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>
+  <div class="flex flex-col items-center">
     <ProductDetail :product="product" v-if="product" />
     <div v-else>Loading...</div>
-    <button @click="goBack" class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      Back to Products
+    <button
+      @click="goBack"
+      class="flex items-center justify-center mt-6 ml-auto bg-gray-200 hover:bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded-full fixed bottom-5 right-5"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-5 h-5 me-2"
+      >
+        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5l-7-7m0 0l7-7m-7 7h16.5"/>
+      </svg>
+      Back to Product List
     </button>
   </div>
 </template>
